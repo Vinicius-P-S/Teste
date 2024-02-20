f3f24440b89276ed1c95fac35df7dc86d64d8b27
Vinicius-P-S <160556347+Vinicius-P-S@users.noreply.github.com>
2024-02-19 23:03:34 -0300

    Delete people-data-main directory

--- people-data-main/people-data/src/main/java/br/com/vpelizzarisilva/peopledata/repository/AddressRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.vpelizzarisilva.peopledata.repository;
-
-import br.com.vpelizzarisilva.peopledata.entitie.AddressEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressRepository extends JpaRepository<AddressEntity, Long> {
-}
